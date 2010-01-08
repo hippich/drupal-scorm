69e7b6db6fb91b4f9ee208cae2a58462ae23e085
Stefan Auditor <stefan.auditor@erdfisch.de>
2010-01-08 11:29:35 +0000

    views reporting basics

---
+++ scorm_reporting.views.inc.php
@@ -0,0 +1,20 @@
+<?php
+//$Id
+
+/**
+ * @file
+ * SCORM 2004 - Reporting
+ *
+ * This module creates a Reporting section for each user.
+ *
+ * @author
+ * Stefan Auditor <stefan.auditor@erdfisch.de>
+ */
+
+/**
+ * Implementation of hook_views_data()
+ */
+function scorm_reporting_views_data() {
+  $data = array();
+  return $data;
+}
